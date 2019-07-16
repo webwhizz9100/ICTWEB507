d0c998a9e1a48b2eb514afdadec8ad17b5b043e2
Nat <x5_452_dark@hotmail.com>
2019-07-16 18:07:17 +1000

    index

--- index.php
+++ index.php
@@ -10,17 +10,17 @@ function listWrapper($content){
     </head>
     <body>
         <h1>Posts</h1>
-        <ul class="row">
-            <? while(have_posts()): the_post();?>
-                <li class="column">
-                    <div class="card">
-                            <h2 class="card_title"><? the_title()?></h2>
-                            <div class="card_content"><? the_content()?></div>
-                            <div class="card_img"><? the_post_thumbnail();?></div>
-                    </div>
-                </li>
-            <? endwhile ?>
-        </ul>
+            <ul class="row">
+                <? while(have_posts()): the_post();?>
+                    <li class="column">
+                        <div class="card">
+                                <h2 class="card_title"><? the_title()?></h2>
+                                <div class="card_content"><? the_content()?></div>
+                                <div class="card_img"><? the_post_thumbnail();?></div>
+                        </div>
+                    </li>
+                <? endwhile ?>
+            </ul>
     </body>
 </html>
 
