4b58b3eb4a120feb5d6e6a0e2e47bfd7c73f43c1
Nat <x5_452_dark@hotmail.com>
2019-07-16 17:46:51 +1000

    changes index.php

--- index.php
+++ index.php
@@ -1,15 +1,27 @@
-<?php
+<!-- <?php
 function listWrapper($content){
     echo '<li>' . $content . '.</li>';
 }
-?>
-<h1>Posts</h1>
-<ul>
-    <? while(have_posts()): the_post();?>
-        <li><? the_title()?></li>
-            <? the_content()?>
-    <? endwhile ?>
-</ul>
-
+?> -->
+<html>
+    <head>
+        <title>foo_knews</title>
+        <link rel="stylesheet" href="<?php echo get_stylesheet_uri()?>" media="screen">
+    </head>
+    <body>
+        <h1>Posts</h1>
+        <ul class="row">
+            <? while(have_posts()): the_post();?>
+                <li class="column">
+                    <div class="card">
+                            <h2 class="card_title"><? the_title()?></h2>
+                            <div class="card_content"><? the_content()?></div>
+                            <div class="card_img"><? the_post_thumbnail();?></div>
+                    </div>
+                </li>
+            <? endwhile ?>
+        </ul>
+    </body>
+</html>
 
 
