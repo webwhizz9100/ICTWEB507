78954c4b9e24f4b0e97abaddb161ea207b269694
Nat <x5_452_dark@hotmail.com>
2019-07-16 19:08:05 +1000

    while loop

--- index.php
+++ index.php
@@ -1,27 +1,38 @@
-<!-- <?php
-function listWrapper($content){
-    echo '<li>' . $content . '.</li>';
-}
-?> -->
+<?php $x = 1; while($x <= 12){ ?>
+                <li>
+                    <article class="card">
+                        <h1>title</h1>
+                        <img src="http://placehold.it/300x200" alt="img">
+                            <p>;Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis enim, sequi aspernatur, numquam qui iusto hic, blanditiis excepturi possimus quibusdam fugiat fugit velit rerum provident. Perspiciatis ex debitis eveniet animi.</p>
+                        <a>read more</a>
+                    </article>
+                </li>
+<?php $x++; } ?>
 <html>
     <head>
-        <title>foo_knews</title>
-        <link rel="stylesheet" href="<?php echo get_stylesheet_uri()?>" media="screen">
+        <title>Foo_Knews | Bizzare news for outer planet</title>
+            <body>
+                <h1>Latest News</h1>
+                <ul>
+                    <article class="card">
+                        <h1>title</h1>
+                        <img src="http://placehold.it/300x200" alt="img">
+                            <p>;Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis enim, sequi aspernatur, numquam qui iusto hic, blanditiis excepturi possimus quibusdam fugiat fugit velit rerum provident. Perspiciatis ex debitis eveniet animi.</p>
+                        <li>post button</li>
+                    </article>
+                    <article class="card">
+                        <h1>title</h1>
+                        <img src="http://placehold.it/300x200" alt="img">
+                            <p>;Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam saepe recusandae fuga aspernatur architecto blanditiis praesentium eveniet. Suscipit in voluptatibus saepe nesciunt eveniet. Illum, dignissimos omnis. Nihil ea commodi dicta?</p>
+                        <li>post button</li>
+                    </article>
+                    <article class="card">
+                        <h1>title</h1>
+                        <img src="http://placehold.it/300x200" alt="img">
+                            <p>;Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus atque sed aspernatur, iusto facilis officiis vero ea, inventore rerum laboriosam minima expedita a, in suscipit dolores beatae! Deserunt, veritatis possimus!</p>
+                        <li>post button</li>
+                    </article>
+                </ul>
+            </body>
     </head>
-    <body>
-        <h1>Posts</h1>
-            <ul class="row">
-                <? while(have_posts()): the_post();?>
-                    <li class="column">
-                        <div class="card">
-                                <h2 class="card_title"><? the_title()?></h2>
-                                <div class="card_content"><? the_content()?></div>
-                                <div class="card_img"><? the_post_thumbnail();?></div>
-                        </div>
-                    </li>
-                <? endwhile ?>
-            </ul>
-    </body>
 </html>
-
-
