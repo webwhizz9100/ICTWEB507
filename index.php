571a5dea79b0cfde482d763687ae1406cdfa9640
Nat <x5_452_dark@hotmail.com>
2019-07-17 18:52:48 +1000

    wordpress functions

--- index.php
+++ index.php
@@ -1,38 +1,21 @@
-<?php $x = 1; while($x <= 12){ ?>
-                <li>
-                    <article class="card">
-                        <h1>title</h1>
-                        <img src="http://placehold.it/300x200" alt="img">
-                            <p>;Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis enim, sequi aspernatur, numquam qui iusto hic, blanditiis excepturi possimus quibusdam fugiat fugit velit rerum provident. Perspiciatis ex debitis eveniet animi.</p>
-                        <a>read more</a>
-                    </article>
-                </li>
-<?php $x++; } ?>
 <html>
     <head>
         <title>Foo_Knews | Bizzare news for outer planet</title>
+    </head>
             <body>
                 <h1>Latest News</h1>
-                <ul>
-                    <article class="card">
-                        <h1>title</h1>
-                        <img src="http://placehold.it/300x200" alt="img">
-                            <p>;Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis enim, sequi aspernatur, numquam qui iusto hic, blanditiis excepturi possimus quibusdam fugiat fugit velit rerum provident. Perspiciatis ex debitis eveniet animi.</p>
-                        <li>post button</li>
-                    </article>
-                    <article class="card">
-                        <h1>title</h1>
-                        <img src="http://placehold.it/300x200" alt="img">
-                            <p>;Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam saepe recusandae fuga aspernatur architecto blanditiis praesentium eveniet. Suscipit in voluptatibus saepe nesciunt eveniet. Illum, dignissimos omnis. Nihil ea commodi dicta?</p>
-                        <li>post button</li>
-                    </article>
-                    <article class="card">
-                        <h1>title</h1>
-                        <img src="http://placehold.it/300x200" alt="img">
-                            <p>;Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus atque sed aspernatur, iusto facilis officiis vero ea, inventore rerum laboriosam minima expedita a, in suscipit dolores beatae! Deserunt, veritatis possimus!</p>
-                        <li>post button</li>
-                    </article>
-                </ul>
+                    <ul class="grid">
+                        <?php while(have_posts()): the_post(); ?>
+                                        <li class="grid-item">
+                                            <article class="card">
+                                                <h1><?php the_title()?></h1>
+                                                <?php the_post_thumbnail('thumbnail');?>
+                                                    <?php the_excerpt()?>
+                                                    <a href="<?php the_permalink()?>">Read More</a>                                           
+                                            </article>
+                                        </li>
+                        <?php endwhile ?>
+                    </ul>
             </body>
-    </head>
 </html>
+        
